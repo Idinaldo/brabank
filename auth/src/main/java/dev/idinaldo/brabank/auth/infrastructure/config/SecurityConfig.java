5467c206e5e4790e84d410578886b5758b318927
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-25 04:58:01 -0300

    refactor: update SecurityConfig to correctly configure PasswordEncoder and allow /api/auth/register endpoint calls

---
+++ auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package dev.idinaldo.brabank.auth.infrastructure.exceptions.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/login").permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        return new BCryptPasswordEncoder();
+    }
+}
