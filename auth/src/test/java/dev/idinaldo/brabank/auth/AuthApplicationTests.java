93fcdb0f91d9f8d858196f7aef3128b8dcdd7f20
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-25 04:05:16 -0300

    feat: add default tests

---
+++ auth/src/test/java/dev/idinaldo/brabank/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.idinaldo.brabank.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
