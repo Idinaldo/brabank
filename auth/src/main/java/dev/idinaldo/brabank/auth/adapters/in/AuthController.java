8f92dee039cd1af218d8e34fe7ce106a056700ff
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-25 05:02:53 -0300

    feat: add AuthController and /api/auth/register endpoint

---
+++ auth/src/main/java/dev/idinaldo/brabank/auth/adapters/in/AuthController.java
@@ -0,0 +1,16 @@
+package dev.idinaldo.brabank.auth.adapters.in;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerIdentity() {
+        return ResponseEntity.ok("Endpoint is up!");
+    }
+}
