7fe8dd2b728bd504eb702e068cd9e2a20e53ca59
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-27 05:56:12 -0300

    feat: add RegisterUseCaseImpl to execute register operation

---
+++ auth/src/main/java/dev/idinaldo/brabank/auth/application/usecases/impl/RegisterUseCaseImpl.java
@@ -0,0 +1,28 @@
+package dev.idinaldo.brabank.auth.application.usecases.impl;
+
+import dev.idinaldo.brabank.auth.adapters.in.dtos.IdentityRequestDTO;
+import dev.idinaldo.brabank.auth.adapters.out.dtos.IdentityResponseDTO;
+import dev.idinaldo.brabank.auth.application.ports.IdentityMapper;
+import dev.idinaldo.brabank.auth.application.ports.out.IdentityRepository;
+import dev.idinaldo.brabank.auth.application.usecases.contracts.RegisterUseCase;
+import dev.idinaldo.brabank.auth.domain.models.Identity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegisterUseCaseImpl implements RegisterUseCase {
+
+    private final IdentityMapper identityMapper;
+    private final IdentityRepository identityRepository;
+
+    public RegisterUseCaseImpl(IdentityMapper identityMapper, IdentityRepository identityRepository) {
+        this.identityMapper = identityMapper;
+        this.identityRepository = identityRepository;
+    }
+
+    @Override
+    public IdentityResponseDTO execute(IdentityRequestDTO identityRequestDTO) {
+        Identity identity = identityMapper.requestDtoToDomain(identityRequestDTO);
+        Identity registeredIdentity = identityRepository.save(identity);
+        return identityMapper.domainToResponseDto(registeredIdentity);
+    }
+}
