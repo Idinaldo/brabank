4627093b6b829a79224f0f991f1adbd12a38d135
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-27 05:37:00 -0300

    feat: move RegisterUseCase to contracts directory

--- auth/src/main/java/dev/idinaldo/brabank/auth/application/usecases/contracts/RegisterUseCase.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/application/usecases/contracts/RegisterUseCase.java
@@ -1,8 +1,8 @@
-package dev.idinaldo.brabank.auth.application.usecases;
+package dev.idinaldo.brabank.auth.application.usecases.contracts;
 
 import dev.idinaldo.brabank.auth.adapters.in.dtos.IdentityRequestDTO;
 import dev.idinaldo.brabank.auth.adapters.out.dtos.IdentityResponseDTO;
 
 public interface RegisterUseCase {
-    IdentityResponseDTO register(IdentityRequestDTO identityRequestDTO);
+    IdentityResponseDTO execute(IdentityRequestDTO identityRequestDTO);
 }
