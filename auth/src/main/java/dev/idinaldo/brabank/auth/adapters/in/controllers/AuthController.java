64cd8132f4f9063c20e5b858c77e25fa28934a06
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-27 05:48:18 -0300

    feat: add business logic to register endpoint and registerUseCase implementation through contract

--- auth/src/main/java/dev/idinaldo/brabank/auth/adapters/in/controllers/AuthController.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/adapters/in/controllers/AuthController.java
@@ -1,7 +1,10 @@
 package dev.idinaldo.brabank.auth.adapters.in.controllers;
 
 import dev.idinaldo.brabank.auth.adapters.in.dtos.IdentityRequestDTO;
+import dev.idinaldo.brabank.auth.application.usecases.contracts.RegisterUseCase;
+import dev.idinaldo.brabank.auth.domain.services.IdentityService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final RegisterUseCase registerUseCase;
+
+    public AuthController(RegisterUseCase registerUseCase) {
+        this.registerUseCase = registerUseCase;
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> registerIdentity(@RequestBody @Valid IdentityRequestDTO identityRequestDTO) {
-        return ResponseEntity.ok("Endpoint is up!");
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerUseCase.execute(identityRequestDTO));
     }
 }
