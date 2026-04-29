ef83732bc0e4bfd7a530067081d4b0ac440f7ef7
Idinaldo <idinaldooliveira220@gmail.com>
2026-04-29 03:06:08 -0300

    feat: import removal

--- auth/src/main/java/dev/idinaldo/brabank/auth/adapters/in/controllers/AuthController.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/adapters/in/controllers/AuthController.java
@@ -2,7 +2,6 @@ package dev.idinaldo.brabank.auth.adapters.in.controllers;
 
 import dev.idinaldo.brabank.auth.adapters.in.dtos.IdentityRequestDTO;
 import dev.idinaldo.brabank.auth.application.usecases.contracts.RegisterUseCase;
-import dev.idinaldo.brabank.auth.domain.services.IdentityService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
