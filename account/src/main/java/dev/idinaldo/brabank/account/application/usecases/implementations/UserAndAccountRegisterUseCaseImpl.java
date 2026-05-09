f7f3ed54ac2a53044fc2696b7370a4636669a9a5
Idinaldo <idinaldooliveira220@gmail.com>
2026-05-09 19:56:48 -0300

    refactor: renamed UserRegisterUseCaseImpl to UserAndAccountRegisterUseCaseImpl

--- account/src/main/java/dev/idinaldo/brabank/account/application/usecases/implementations/UserAndAccountRegisterUseCaseImpl.java
+++ account/src/main/java/dev/idinaldo/brabank/account/application/usecases/implementations/UserAndAccountRegisterUseCaseImpl.java
@@ -1,14 +1,13 @@
 package dev.idinaldo.brabank.account.application.usecases.implementations;
 
-import dev.idinaldo.brabank.account.application.usecases.contracts.UserRegisterUseCase;
+import dev.idinaldo.brabank.account.application.usecases.contracts.UserAndAccountRegisterUseCase;
 import dev.idinaldo.brabank.account.domain.models.User;
 import dev.idinaldo.brabank.account.infrastructure.exceptions.CpfAlreadyInUseException;
 import dev.idinaldo.brabank.account.infrastructure.exceptions.EmailAlreadyInUseException;
 import dev.idinaldo.brabank.account.infrastructure.persistence.UserRepositoryPort;
 
-// TODO: review exceptions as exposing
-//  registered data is a bad cybersecurity practice
-public class UserRegisterUseCaseImpl implements UserRegisterUseCase {
+// TODO: review exceptions for cybersecurity concerns (account mapping, data exposure)
+public class UserAndAccountRegisterUseCaseImpl implements UserAndAccountRegisterUseCase {
 
     private UserRepositoryPort userRepositoryPort;
 
