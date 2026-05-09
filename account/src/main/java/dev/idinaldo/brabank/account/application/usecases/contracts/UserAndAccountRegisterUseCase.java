6b915b2cf5c79d228ed037539793533b75402a40
Idinaldo <idinaldooliveira220@gmail.com>
2026-05-09 19:57:35 -0300

    refactor: rename UserRegisterUseCase to UserAndAccountRegisterUseCase

--- account/src/main/java/dev/idinaldo/brabank/account/application/usecases/contracts/UserAndAccountRegisterUseCase.java
+++ account/src/main/java/dev/idinaldo/brabank/account/application/usecases/contracts/UserAndAccountRegisterUseCase.java
@@ -2,6 +2,6 @@ package dev.idinaldo.brabank.account.application.usecases.contracts;
 
 import dev.idinaldo.brabank.account.domain.models.User;
 
-public interface UserRegisterUseCase {
+public interface UserAndAccountRegisterUseCase {
     public void execute(User user);
 }
