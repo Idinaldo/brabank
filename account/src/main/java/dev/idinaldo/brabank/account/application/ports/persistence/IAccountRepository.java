32e7963a284e444cb8e5639bea7faa9381f320bd
Idinaldo <idinaldooliveira220@gmail.com>
2026-05-10 02:40:31 -0300

    refactor: rename AccountRepositoryPort to IAccountRepository for clarification and alignment with best practices

--- account/src/main/java/dev/idinaldo/brabank/account/application/ports/persistence/IAccountRepository.java
+++ account/src/main/java/dev/idinaldo/brabank/account/application/ports/persistence/IAccountRepository.java
@@ -1,10 +1,10 @@
-package dev.idinaldo.brabank.account.infrastructure.persistence;
+package dev.idinaldo.brabank.account.application.ports.persistence;
 
 import dev.idinaldo.brabank.account.domain.models.Account;
 
 import java.util.UUID;
 
-public interface AccountRepositoryPort {
+public interface IAccountRepository {
 
     Account save(Account account);
     Account findByAccountNumber(String accountNumber);
