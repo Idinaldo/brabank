c3427fdf0264f252c0277eb4eec071ab0b3bab89
Idinaldo <idinaldooliveira220@gmail.com>
2026-05-10 02:57:00 -0300

    refactor: update Status.java to represent in a clearer way the statuses of an account

--- account/src/main/java/dev/idinaldo/brabank/account/domain/valueObjects/Status.java
+++ account/src/main/java/dev/idinaldo/brabank/account/domain/valueObjects/Status.java
@@ -1,8 +1,8 @@
 package dev.idinaldo.brabank.account.domain.valueObjects;
 
 public enum Status {
-    PENDING_VERIFICATION,
-    ACTIVE,
-    DEACTIVATED,
-    BLOCKED
+    PENDING_KYC_VERIFICATION, // registered but didn't complete KYC
+    ACTIVE, // regular account with KYC complete
+    DEACTIVATED, // regular account that was deactivated by client
+    BLOCKED // brabank blocked client account
 }
